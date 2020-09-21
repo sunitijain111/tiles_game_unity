5e1073c532b3998acc4feae821f0b6ea86593414
SunitiJain111 <suniti135btcse17@gmail.com>
2020-09-22 00:13:30 +0530

    adding game

---
+++ tiles1 2D/Assets/scripts/MenuBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void triggerMenuBehavior(int i)
+  {
+    switch(i)
+   {
+     default :
+case(0):
+ SceneManager.LoadScene("level1");
+break;
+case(1):
+Application.Quit();
+break;
+   }
+  }
+}
